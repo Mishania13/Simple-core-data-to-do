7019a588db900372d7f8277c11c510e656da0aa1
Mishania13 <mishk777@yandex.ru>
2020-05-07 12:32:48 +0300

    Initial Commit

---
+++ DemoCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoCoreData
+//
+//  Created by Mr. Bear on 07.05.2020.
+//  Copyright © 2020 Mr. Bear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
